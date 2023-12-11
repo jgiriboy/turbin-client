c935380498b46cae5ff194573f4cb5362d7b0f1d
Gieun Jeong <gieun.jeong@kaist.ac.kr>
2023-12-12 02:46:38 +0900

    feat: qr reward

--- src/components/qrPage/Html5QrcodePlugin.js
+++ src/components/qrPage/Html5QrcodePlugin.js
@@ -1,7 +1,17 @@
 import { Html5Qrcode } from 'html5-qrcode';
 import { useEffect } from 'react';
+import axios from 'axios';
+
+const SERVER_URL =
+    'https://port-0-turbin-server-cn1vmr2clpfb98ea.sel5.cloudtype.app/';
+// const SERVER_URL = 'http://localhost:8000/';
 
 const Html5QrcodePlugin = () => {
+    let TOTAL_REWARD;
+    axios.get(SERVER_URL + 'reward').then((res) => {
+        TOTAL_REWARD = res.data.reward;
+    });
+
     let scanning = false;
     let html5Qrcode;
 
@@ -33,7 +43,35 @@ const Html5QrcodePlugin = () => {
     }
 
     async function onScanSuccess(decodedText, decodedResult) {
-        console.log(decodedText);
+        if (decodedText == 'reward') {
+            const userReward = Math.floor(Math.random() * 100);
+            const response = await axios.post(SERVER_URL + decodedText, {
+                userid: 1,
+                value: userReward,
+            });
+            const namelist = [
+                '인하아리스타',
+                '기은 하우스',
+                '하이테크센터',
+                '인하대 후문',
+                '인하대 정문',
+                '인하대학교본관',
+                '인하대역헤리움오피스텔',
+                '힐스테이트학익',
+                '하이테크센터',
+                '인하대학교1생활관',
+            ];
+            await axios.post(SERVER_URL + 'new-reward', {
+                userid: 1,
+                reward: userReward,
+                username: 'gieun jeong',
+                turbinName: namelist[Math.floor(Math.random() * 10)],
+                totalReward: TOTAL_REWARD + userReward,
+            });
+            alert('리워드 적립!');
+        } else {
+            alert('유효한 QR이 아닙니다');
+        }
         scanning = false;
     }
 
--- src/components/rewardPage/RewardPage.js
+++ src/components/rewardPage/RewardPage.js
@@ -2,8 +2,27 @@ import './RewardPage.css';
 import { Link } from 'react-router-dom';
 import SavedBudget from '../infoPage/SavedBudget';
 import RewardLog from './RewardLog';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const SERVER_URL =
+    'https://port-0-turbin-server-cn1vmr2clpfb98ea.sel5.cloudtype.app/';
 
 const RewardPage = () => {
+    const [rewardHistory, setRewardHistory] = useState([
+        { totalReward: 0, reward: 0, last_updated: '', turbinName: '' },
+    ]);
+
+    const fetchHistory = async () => {
+        const response = await axios.get(SERVER_URL + 'new-reward');
+        setRewardHistory(response.data);
+        console.log(response.data);
+    };
+
+    useEffect(() => {
+        fetchHistory();
+    }, []);
+
     return (
         <div className='reward-page'>
             <div className='reward-nav'>
@@ -22,13 +41,24 @@ const RewardPage = () => {
                 <div></div>
             </div>
             <SavedBudget
-                budget={1595}
+                budget={rewardHistory[rewardHistory.length - 1].totalReward}
                 title={'이달'}
                 noSpec={false}
                 description='모은 리워드'
                 pointUnits='포인트'
             />
-            <RewardLog
+            {rewardHistory.reverse().map((currentData) => {
+                return (
+                    <RewardLog
+                        key={Math.floor(Math.random() * 1000)}
+                        totalBudget={currentData.totalReward}
+                        newBudget={currentData.reward}
+                        newDate={currentData.last_updated}
+                        newTitle={currentData.turbinName}
+                    />
+                );
+            })}
+            {/* <RewardLog
                 totalBudget={1595}
                 newBudget={495}
                 newDate='2023. 11. 28. 10:29:49'
@@ -45,7 +75,7 @@ const RewardPage = () => {
                 newBudget={500}
                 newDate='2023. 11. 25. 13:49:35'
                 newTitle='하이테크센터'
-            />
+            /> */}
         </div>
     );
 };
